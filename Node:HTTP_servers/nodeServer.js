4b8317be6db0be0e2f77dd7cac30b2cdd3074f7d
School <school@Mateo.attlocal.net>
2019-01-17 22:56:49 -0500

    next video

--- Node:HTTP_servers/nodeServer.js
+++ Node:HTTP_servers/nodeServer.js
@@ -1,21 +1,45 @@
 const http = require('http');
 
+
+// fs = file system module. fs is built into Node. we just have to ask for it.
+// fs gives access to THIS computers file system.
+const fs = require('fs')
+
 // the http module has a createServer method
 // takes 1 arg:
 // 1. callback, callback, has 2 args: args: req, res 
 const server = http.createServer((req, res)=>{
-    // console.log(req)
-    // res = our way of responding to the requester 
-    // http message 
-    // 1. start-line - CHECK
-    // 2. header
-    // 3. body
-    // writeHead takes 2 args:
-    // 1. status code
-    // 2. object for the mime-type 
-    res.writeHead(200,{'content-type':'text/html'});
-    res.write('<h1>Testing Testing Testing!</h1>');
-    res.end();
+    if(req.url === '/'){
+        // the user wants the home page. we know becuase the req object has / in the url property
+        // console.log(req)
+        // res = our way of responding to the requester 
+        // http message 
+        // 1. start-line - CHECK
+        // 2. header
+        // 3. body
+        // writeHead takes 2 args:
+        // 1. status code
+        // 2. object for the mime-type 
+        res.writeHead(200,{'content-type':'text/html'});
+        // res.write();
+        const homePageHTML = fs.readFileSync('node.html')
+        res.write(homePageHTML)
+        res.end();
+    }else if(req.url === "/node.png"){
+        res.writeHead(200,{'content-type':'image/png'});
+        const image = fs.readFileSync('node.png')
+        res.write(image)
+        res.end();
+    }else if(req.url === "/styles.css"){
+        res.writeHead(200,{'content-type':'text/css'});
+        const css = fs.readFileSync('styles.css')
+        res.write(css)
+        res.end();
+    }else{
+        res.writeHead(404,{'content-type':'text/html'});
+        res.write('<h4>Dead End</h4>');
+        res.end();
+    }
 });
 
 // createServer returns an object with a listen method 
